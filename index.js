e6439345f111a65d00758cf0a6011688cef6b31a
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-08-24 09:21:52 -0500

    shape classes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const svgsite = "http://www.w3.org/2000/svg";
+const { square, triangle, circle } = require("./lib/shapes");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-function createLogo(shape, shapeColor, text, textColor) {
-  let newLogo = document.createElement(svgsite, shape);
-}
+fs.writeFile(
+  "log.txt",
+  function createLogo(shape, shapeColor, text, textColor) {
+    let newLogo = document.createElementNS(svgsite, shape);
+  }
+);
